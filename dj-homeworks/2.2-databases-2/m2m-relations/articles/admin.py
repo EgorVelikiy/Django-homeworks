e797660b40c5f70e794dbd790758c1ba30241c49
Egor Velikiy <egor.velikiy.02@mail.ru>
2023-12-06 18:04:26 +0300

    m2m

--- dj-homeworks/2.2-databases-2/m2m-relations/articles/admin.py
+++ dj-homeworks/2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope, Tag
+
+
+class ScopeInLineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_tag = 0
+        for form in self.forms:
+            if form.cleaned_data != {}:
+                main_tag += form.cleaned_data['is_main']
+            elif main_tag > 1:
+                raise ValidationError('Главный тег должен быть только один!')
+                break
+        return super().clean()
+
+
+class ScopeInLine(admin.TabularInline):
+    model = Scope
+    extra = 1
+    formset = ScopeInLineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ScopeInLine]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    inlines = [ScopeInLine]
